b8e184a60418f1068eb229f2443d6a512bb8056f
brenoMatias <matiasbreno19@gmail.com>
2021-12-24 04:34:19 -0300

    setting updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="main-content">
+      <div> 
+        <h1>Hello World</h1>   
+      </div>
+      
   
     </div>
   );
