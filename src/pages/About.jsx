573651eda3fcc80cced6ae34f9edca25be30e9a0
brenoMatias <matiasbreno19@gmail.com>
2021-12-24 13:40:21 -0300

    final backup

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import '../App.css';
 
 function About() {
     return (
+        <div className="about"> 
         <div className="about-main">
             <h1>test</h1>
+        </div>
         </div>
     )
 }
